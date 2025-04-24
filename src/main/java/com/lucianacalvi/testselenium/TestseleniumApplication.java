59a596e4c9c9f6bc77047ab4d3086abf09104fb4
Luciana Calvi <lucalvi64@gmail.com>
2025-04-24 19:17:38 -0300

    Proyecto con selenium java

---
+++ src/main/java/com/lucianacalvi/testselenium/TestseleniumApplication.java
@@ -0,0 +1,13 @@
+package com.lucianacalvi.testselenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestseleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestseleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianacalvi/testselenium/TestseleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianacalvi.testselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestseleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
